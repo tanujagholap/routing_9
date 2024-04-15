3375fa2503f7e3c72176ecf7ca658d0c1688c4a3
demo1 <tanujarbhong@gmail.com>
2024-04-15 14:32:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './compo/Layout/Navbar';
+import Navbar from './compo/layout/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './compo/Pages/Home'
-import About from './compo/Pages/About'
-import Service from './compo/Pages/Service'
-import Register from './compo/Pages/Register'
-import Errorpage from './compo/Errorpage'
 import Add from './compo/Pages/Add'
 import Show from './compo/Pages/Show'
 import Update from './compo/Pages/Update.js'
@@ -22,16 +17,12 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-            <Route path='/home' element={<Home/>}></Route>
-            <Route path='/about' element={<About/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-            <Route path='/service' element={<Service/>}></Route>
+          <Route path='/' element={<Add/>}></Route>
             <Route path='/add' element={<Add/>}></Route>
             <Route path='/show' element={<Show/>}></Route>
             <Route path='/update/:userId' element={<Update/>}></Route>
             <Route path='/delete/:userId' element={<Delete/>}></Route>
-            <Route path='*' element={<Errorpage/>}></Route>
+            
         </Routes>
       </BrowserRouter>
     
